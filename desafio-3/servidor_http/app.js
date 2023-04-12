279df385753be4f44f69ecd2dba8baa196a8cb89
Carolina Yut <yut.carolina@gmail.com>
2023-04-12 00:26:23 -0300

    actualizacion

--- desafio-3/servidor_http/app.js
+++ desafio-3/servidor_http/app.js
@@ -1,17 +1,18 @@
+const fs = require ('fs');
 const express = require ('express');
+const { productManager } = ('./productos');
 
 const PUERTO = 8080;
 
 const server = express();
 server.use(express.json);
+server.use (express.urlencoded({extended:true}));
 
-server.use (express.urlencoded({extended:true}))
+const newProduct = new productManager; 
 
-
-
-server.get ( '/saludo', (req,res) => {
-	res.send('saludo desde el server express');
-	
+server.get ( '/products', (req,res) => {
+	const mostrarProducto = newProduct.getProducts 
+    res.send(mostrarProducto);
 } );
 
 server.listen(PUERTO, () => {
