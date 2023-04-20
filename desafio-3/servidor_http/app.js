558bbd4547a037a9b9d1f65a8aecf5c262dee510
Carolina Yut <yut.carolina@gmail.com>
2023-04-20 18:58:20 -0300

    Actualizacion

--- desafio-3/servidor_http/app.js
+++ desafio-3/servidor_http/app.js
@@ -1,17 +1,40 @@
-import express from "express";
-import productManager from "./components/productos.js";
+import express, { query } from "express";
+import ProductManager from "./components/productos.js";
 
-const server = express();
-server.use(express.urlencoded({extended:true}));
+const app = express();
+app.use(express.urlencoded({extended:true}));
 
-const productos = new productManager(); 
-const leerProductos = productos.getProducts();
+const productos = new ProductManager(); 
+const leerProductos = productos.readProducts();
 console.log (await leerProductos);
 
-/*
-server.listen(PUERTO, () => {
-	console.log(`Servidor express activo en puerto ${PUERTO}`);
+app.get("/products", async (req,res)=> {
+	let limit = parseInt(req.query.limit);
+	if (!limit) return res.send(await leerProductos) // si es distinto me trae todo los productos
+	let allProducts = await leerProductos; // me trae todos los productos
+	let productLimit = allProducts.slice(0, limit) // slice hace una copia del array y nos muestra una parte
+	res.send(productLimit);  
+}); 
+
+app.get("/products/:id", async (req,res)=> {
+	let id = parseInt (req.params.id);
+	let allProducts = await leerProductos;
+	let productosById = allProducts.find(product => product.id === id);
+	res.send(productosById);
 });
 
-const PUERTO = 8080;
+const PORT = 8080;
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Server is running. http://localhost:${PORT}`)
+})
+
+
+/*
+TEST
+http://localhost:8080/products
+http://localhost:8080/products/?limit=5
+http://localhost:8080/products/5
 */
--- desafio-3/servidor_http/components/productos.js
+++ desafio-3/servidor_http/components/productos.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-
+/* const fs = require('fs');
 export default class productManager {
     constructor() {
         this.productos = [];
@@ -150,3 +149,64 @@ productos.addProduct('producto10','es un producto 10', 12, 'sin foto', '100',140
     }
   ]
   */
+
+  
+
+import { promises as fs} from "fs";
+export default class ProductManager {
+constructor () {
+ this.patch = "./productos.json";
+ this.products = [];
+}
+
+static id = 0;
+
+addProduct = async (title,description,price,imagen,code,stock) => {
+
+ProductManager.id++;
+let newProduct = {
+title,
+description,
+price,
+imagen,
+code,
+stock,
+id: ProductManager.id,
+};
+
+this.products.push(newProduct);
+
+await fs.writeFile(this.patch, JSON.stringify(this.products))
+};
+
+readProducts = async () => {
+let respuesta = await fs.readFile (this.patch, "utf-8");
+return JSON.parse (respuesta);
+};
+
+getProducts = async () => {
+};
+
+getProductsById = async (id) => {
+};
+
+
+deleteProductsById = async (id) => {
+
+let respuesta3 = await this.readProducts();
+let productFilter = respuesta3.filter ((products) => products.id != id);
+await fs.writeFile (this.patch, JSON.stringify (productFilter));
+console.log ("Producto eliminado")
+
+};
+
+updateProducts = async ( {id, ...producto}) => {
+
+await this.deleteProductsById (id);
+let productoId = await this.readProducts ();
+let productosModif = [{...producto,id}, ...productoId];
+await fs.writeFile (this.patch, JSON.stringify (productosModif));
+};
+}
+
+//const productos = new ProductManager ();
